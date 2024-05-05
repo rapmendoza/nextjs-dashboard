6337163f676f224f5669466a1ed7a27a1db50aca
Rap Mendoza <smendoza@captivatechat.com>
2024-05-05 12:18:42 +0800

    Define base url

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lutisana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700']
+});
